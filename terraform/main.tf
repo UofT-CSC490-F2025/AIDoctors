5ac9c13080aa2302de281e69b0d82544c26ca668
TerryTian21 <terry.tian.2003@gmail.com>
2025-10-27 23:23:41 -0400

    Test Run

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,24 @@
-provider "aws" {
-    region = "us-east-1"
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+    backend "s3" {
+        bucket         = "aidoctors-terraform-state"
+        key            = "prod/terraform.tfstate"
+        region         = "us-east-2"
+        encrypt        = true
+        use_lockfile   = true
+    }
 }
+
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "test" {
+    bucket = "test-bucket"
+}
+    
