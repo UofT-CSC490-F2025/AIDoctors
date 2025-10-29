9cb6c5a5dbe5fd1ee9ab720e2ca723caa6360bd7
TerryTian21 <terry.tian.2003@gmail.com>
2025-10-29 00:41:01 -0400

    Update DB AZ

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -5,9 +5,9 @@ module "vpc" {
     cidr = "10.0.0.0/16"
 
     azs = ["us-east-2a", "us-east-2b", "us-east-2c"]
-    public_subnets = ["10.0.1.0/24"]
-    private_subnets = ["10.0.2.0/24"]
-    database_subnets = ["10.0.3.0/24"]
+    public_subnets = ["10.0.1.0/24", "10.0.4.0/24"]
+    private_subnets = ["10.0.2.0/24", "10.0.5.0/24"]
+    database_subnets = ["10.0.3.0/24", "10.0.6.0/24"]
 
     enable_nat_gateway = true
     enable_dns_support   = true
