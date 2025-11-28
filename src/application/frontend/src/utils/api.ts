10e60745ff50d90c2cbc854c0f02fb8774fec0dd
Rayce Ramsay <ramsayrayce@gmail.com>
2025-11-28 09:06:51 -0500

    Dummy commit

--- src/application/frontend/src/utils/api.ts
+++ src/application/frontend/src/utils/api.ts
@@ -1,6 +1,7 @@
 const _DEFAULT_API_BASE =
   process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000';
 
+// dummy comment to trigger CI
 export function getApiBaseUrl() {
   return _DEFAULT_API_BASE.replace(/\/$/, '');
 }
