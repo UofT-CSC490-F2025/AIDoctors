4f434cfada79f18016fdbf101518441405ef9e55
TerryTian21 <terry.tian.2003@gmail.com>
2025-12-01 11:07:39 -0500

    update health check route

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -40,7 +40,7 @@ module "alb" {
         enabled             = true
         healthy_threshold   = 2
         interval            = 30
-        path                = "/api/health"
+        path                = "/health"
         port                = "traffic-port"
         protocol            = "HTTP"
         timeout             = 5
